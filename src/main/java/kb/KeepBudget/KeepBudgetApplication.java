3afa746dc715f5232953df2be42579601599cda5
kihyun.kwon <kkh3272@naver.com>
2024-08-12 00:34:05 +0900

    feat: init commit

---
+++ src/main/java/kb/KeepBudget/KeepBudgetApplication.java
@@ -0,0 +1,13 @@
+package kb.KeepBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kb/KeepBudget/KeepBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package kb.KeepBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
